ff1829e43effe69558fa3995382b3a67f80d6154
Stephen Anti <stephenanti63@gmail.com>
2026-08-10 00:14:49 -0700

    1-auth repo

---
+++ backend/modules/auth/repositories/authRepository.js
@@ -0,0 +1,31 @@
+import User from "../models/user.js";
+import Role from "../models/role.js";
+import RefreshToken from "../models/refreshToken.js";
+import VerificationToken from "../models/verificationToken.js";
+
+class AuthRepository {
+  async findUserByEmail(email) {
+    return await User.findOne({ email })
+      .select("+password")
+      .populate("role");
+  }
+
+  async createUser(payload) {
+    return await User.create(payload);
+  }
+
+  async findRoleByName(name) {
+    return await Role.findOne({name});
+  }
+
+  async createRefreshToken(payload) {
+    return await RefreshToken.create(payload);
+  }
+
+  async createVerificationToken(payload) {
+    return await VerificationToken.create(payload);
+  }
+
+}
+
+export default new AuthRepository();
